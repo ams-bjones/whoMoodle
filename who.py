d2066699bfb8375da420b63195a3f4f6658e0065
Brian Jones <bjones@ashmanorschool.com>
2014-03-17 10:21:09 +0000

    Update who.py

--- who.py
+++ who.py
@@ -1,5 +1,10 @@
-stringURL = "http://moodle.ashmanorschool.com/user/profile.php?id="
+import urllib.request as request
+tringURL = "http://moodle.ashmanorschool.com/user/profile.php?id="
 
 for i in range (10000):
     url = stringURL + i
-    print (
+    webpage = request.urlopen(url)
+    start = webpage.find(<title>) +7
+    finish = </title>
+    title = webpage[start:finish]
+    print (title)
