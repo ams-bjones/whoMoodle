9d478e6981f4529dd2b55e677e527c4ed729d285
Brian Jones <bjones@ashmanorschool.com>
2014-03-18 08:26:58 -0400

    updateed for storage

--- who.py
+++ who.py
@@ -1,10 +1,47 @@
-import urllib.request as request
-tringURL = "http://moodle.ashmanorschool.com/user/profile.php?id="
+import urllib2 as request
+import cookielib, urllib, urllib2, getpass
 
-for i in range (10000):
-    url = stringURL + i
+stringURL = "http://moodle.ashmanorschool.com/user/profile.php?id="
+loginUrl = "https://moodle.ashmanorschool.com/login/index.php"
+
+username= raw_input("\tusername: ")
+password= getpass.getpass("\tpassword: ")
+
+
+
+passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
+
+passman.add_password(None, loginUrl, username, password)
+
+cj = cookielib.CookieJar()
+opener = urllib2.build_opener(
+    urllib2.HTTPRedirectHandler(),
+    urllib2.HTTPBasicAuthHandler(passman),
+    urllib2.HTTPHandler(debuglevel=0),
+    urllib2.HTTPSHandler(debuglevel=0),
+    urllib2.HTTPCookieProcessor(cj),
+)
+
+urllib2.install_opener(opener)
+
+
+# set headers
+opener.addheaders = [
+    ('User-agent', ('Mozilla/4.0 (compatible; MSIE 6.0; '
+                   'Windows NT 5.2; .NET CLR 1.1.4322)'))
+]
+
+try:
+    req = opener.open(loginUrl)
+except IOError, e:
+    print "It looks like the username or password is wrong."
+
+
+for i in range (100):
+    url = stringURL + str(i)
     webpage = request.urlopen(url)
-    start = webpage.find(<title>) +7
-    finish = </title>
+    webpage = webpage.read(100)
+    start = webpage.find('<title>') +7
+    finish = webpage.find('</title>')
     title = webpage[start:finish]
-    print (title)
+    print (str(i) + ' ' + title)
